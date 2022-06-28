e344094559d4ae4ecba36fe84383e78700b77dcd
Zakaria Ait Ali <108273189+ZakariaAitAli@users.noreply.github.com>
2022-06-28 20:06:14 +0100

    C

---
+++ C/2-PlayingWithCharachters.c
@@ -0,0 +1,23 @@
+//Task
+//You have to print the character, ch, in the first line.
+// Then print s in next line. In the last line print the sentence, .
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch, s[100] ;
+    
+    scanf("%c", &ch);
+    printf("%c \n", ch);    
+    scanf("%s", &s);
+    printf("%s \n", s);
+    scanf("\n");
+    scanf("%[^\n]%*c", s);
+    printf("%s \n", s);  
+    return 0; 
+}
