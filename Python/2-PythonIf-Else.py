8dd4a35ced7acbd6e3567494deb95cb30311c904
Zakaria Ait Ali <108273189+ZakariaAitAli@users.noreply.github.com>
2022-06-28 13:21:55 +0100

    Python with HackerRank

---
+++ Python/2-PythonIf-Else.py
@@ -0,0 +1,17 @@
+""" Task
+Given an integer, , perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+Input Format
+
+A single line containing a positive integer, . """
+
+n = int(input())
+
+if n%2==0 and (n in range(2,6) or n>20 ):
+    print ("Not Weird"),
+else: 
+    print ("Weird")
